589971251a30bbc8514fc98516a611c8e43257d9
meisterjustice <eziefulejustice@gmail.com>
2023-05-01 13:10:46 +0100

    add scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,16 @@ import { Button, Header } from "@/components";
 import Pagination from "@/components/Pagination";
 import QuestComponent from "@/components/Quest";
 import Footer from "@/components/Footer";
+import Link from "next/link";
 
 export default function Home() {
+  const scroll = () => {
+    window.scrollBy({
+      top: 10000, // could be negative value
+      left: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <div className="w-full">
       <Header />
@@ -14,7 +22,9 @@ export default function Home() {
             <h2 className="text-white mb-4 font-bold text-xl px-8 pt-4 xl:pt-0 xl:px-0 xl:text-6xl xl:text-4xl text-center font-Poppins">
               WORLD FIRST PLAY-TO-EARN POOL GAME
             </h2>
-            <Button px="px-4" text="Play Bit Pool Web" />
+            <Link href="#quest">
+              <Button onClick={scroll} px="px-4" text="Test2Earn" />
+            </Link>
           </div>
         </div>
       </div>
@@ -26,7 +36,10 @@ export default function Home() {
           and other Crypto
         </p>
 
-        <div className="mt-5 xl:gap-11 flex w-full flex-col xl:flex-row  items-start justify-between">
+        <div
+          id="quest"
+          className="mt-5 xl:gap-11 flex w-full flex-col xl:flex-row  items-start justify-between"
+        >
           <div className="flex w-full flex-col gap-2">
             {[1, 2, 3, 4].map((item, index) => (
               <QuestComponent key={index} index={index} />
