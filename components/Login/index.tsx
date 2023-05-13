c4b53bd0bbf7c44463860850aeb01bd29197e66b
crazy0629 <webkiller0104@gmail.com>
2023-05-13 03:06:38 -0700

    fix bug for uploading vercel

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 import Image from "next/image";
 import axios from "axios";
 import * as yup from "yup";
-import jwtDecode from 'jwt-decode';
+import jwtDecode from "jwt-decode";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { notification } from 'antd';
+import { notification } from "antd";
 
 import Input from "../Input";
 import Button, { butonTypes, variantTypes } from "../Button";
 
-import { authActions } from '../../store/auth';
+import { authActions } from "../../store/auth";
 import { SERVER_URI } from "../../config";
 
 import { Check } from "@/public/icons";
@@ -23,8 +23,13 @@ const schema = yup.object().shape({
   password: yup.string().required("Password is required"),
 });
 
-const Login = () => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm<any>({ resolver: yupResolver(schema) });
+const Login = (props: any) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<any>({ resolver: yupResolver(schema) });
 
   const [remember, setRemember] = useState(false);
   const dispatch = useDispatch();
@@ -32,12 +37,19 @@ const Login = () => {
   const onSubmit = async (data: any) => {
     axios.post(`${SERVER_URI}/signin`, data).then((res) => {
       if (res.data.success) {
-        notification.success({ message: 'Success!', description: "You're signed successfully!" })
-        localStorage.setItem('token', res.data.token);
+        notification.success({
+          message: "Success!",
+          description: "You're signed successfully!",
+        });
+        localStorage.setItem("token", res.data.token);
         dispatch(authActions.setCurrentUser(jwtDecode(res.data.token)));
+        props.close();
         reset();
       } else {
-        notification.warning({ message: 'Error!', description: res.data.message });
+        notification.warning({
+          message: "Error!",
+          description: res.data.message,
+        });
       }
     });
   };
@@ -48,19 +60,44 @@ const Login = () => {
       </p>
 
       <form onSubmit={handleSubmit(onSubmit)} className="mt-10">
-        <Input name="email" label="EMAIL" register={register("email")} error={errors.email?.message} placeholder="Enter your email" />
-        <Input name="password" label="PASSWORD" register={register("password")} error={errors.password?.message} placeholder="Enter your password" type="password" />
+        <Input
+          name="email"
+          label="EMAIL"
+          register={register("email")}
+          error={errors.email?.message}
+          placeholder="Enter your email"
+        />
+        <Input
+          name="password"
+          label="PASSWORD"
+          register={register("password")}
+          error={errors.password?.message}
+          placeholder="Enter your password"
+          type="password"
+        />
 
         <div className="mt-10 flex items-center gap-4">
-          <div onClick={() => setRemember(!remember)} className={`h-8 w-8 rounded transition-all duration-300 flex justify-center cursor-pointer items-center ${
-              !remember ? "border border-primary-750 bg-transparent" : "bg-secondary-100 border border-secondary-100" }`} >
+          <div
+            onClick={() => setRemember(!remember)}
+            className={`h-8 w-8 rounded transition-all duration-300 flex justify-center cursor-pointer items-center ${
+              !remember
+                ? "border border-primary-750 bg-transparent"
+                : "bg-secondary-100 border border-secondary-100"
+            }`}
+          >
             {remember && <Check />}
           </div>
           <p className="text-xl text-white font-medium">Remember me</p>
         </div>
 
         <div className="lg:mt-12 mt-10 w-full">
-          <Button variant={variantTypes.full} isFull type={butonTypes.submit} px="px-4" text="SIGN IN" />
+          <Button
+            variant={variantTypes.full}
+            isFull
+            type={butonTypes.submit}
+            px="px-4"
+            text="SIGN IN"
+          />
         </div>
       </form>
 
@@ -74,7 +111,13 @@ const Login = () => {
           <div className="lg:text-xl text-base text-white font-light">
             POWERED BY
           </div>
-          <Image priority={true} height={30} width={130} src={Logo} alt="logo" />
+          <Image
+            priority={true}
+            height={30}
+            width={130}
+            src={Logo}
+            alt="logo"
+          />
         </div>
       </div>
     </div>
