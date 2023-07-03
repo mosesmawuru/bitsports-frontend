262c7d45efbdfd398f5c1d99527dde01ab74850d
stanislav kohut <ctiptopper@gmail.com>
2023-07-04 02:59:55 +0900

    new

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 // export const SERVER_URI = "https://api.bitpool.gg/api";
-export const SERVER_URI = "http://192.168.112.89:8000/api";
-// export const SERVER_URI = "https://bitpool-backend.onrender.com/api";
+// export const SERVER_URI = "http://192.168.112.89:8000/api";
+export const SERVER_URI = "https://bitpool-backend.onrender.com/api";
